01be51d824b35d7e8ffd12055537b1c5e730bee2
Daniel Fergisz <daniel.fergisz@gmail.com>
2020-07-11 21:46:38 +0200

    v1.01 mod output file name

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,22 +22,58 @@ Partial Class Form1
     'Nie należy modyfikować za pomocą edytora kodu.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.StartBF = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.hash = New System.Windows.Forms.TextBox()
+        Me.Restore = New System.Windows.Forms.Button()
         Me.HC = New System.Windows.Forms.ComboBox()
         Me.xNC1 = New System.Windows.Forms.ComboBox()
         Me.xNC2 = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.comSent = New System.Windows.Forms.TextBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Benchmark = New System.Windows.Forms.Button()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.saveInfo = New System.Windows.Forms.Label()
+        Me.clearMailConfig = New System.Windows.Forms.Button()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.M7 = New System.Windows.Forms.TextBox()
+        Me.saveMailData = New System.Windows.Forms.Button()
+        Me.M6 = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.M5 = New System.Windows.Forms.TextBox()
+        Me.M4 = New System.Windows.Forms.TextBox()
+        Me.M3 = New System.Windows.Forms.TextBox()
+        Me.M2 = New System.Windows.Forms.TextBox()
+        Me.M1 = New System.Windows.Forms.TextBox()
+        Me.mailData = New System.Windows.Forms.RichTextBox()
+        Me.TimerSaveOK = New System.Windows.Forms.Timer(Me.components)
+        Me.Log = New System.Windows.Forms.RichTextBox()
+        Me.fileCodCheck = New System.Windows.Forms.Timer(Me.components)
+        Me.Date_Now = New System.Windows.Forms.TextBox()
+        Me.DesCode = New System.Windows.Forms.RichTextBox()
+        Me.FileL = New System.Windows.Forms.TextBox()
+        Me.Datetxt = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
         Me.SuspendLayout()
         '
         'StartBF
         '
-        Me.StartBF.Location = New System.Drawing.Point(478, 27)
+        Me.StartBF.Font = New System.Drawing.Font("Calibri", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.StartBF.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.StartBF.Location = New System.Drawing.Point(485, 27)
         Me.StartBF.Name = "StartBF"
         Me.StartBF.Size = New System.Drawing.Size(75, 26)
         Me.StartBF.TabIndex = 0
@@ -49,9 +85,9 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.hash)
         Me.GroupBox1.Controls.Add(Me.StartBF)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 59)
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 6)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(576, 75)
+        Me.GroupBox1.Size = New System.Drawing.Size(576, 74)
         Me.GroupBox1.TabIndex = 1
         Me.GroupBox1.TabStop = False
         '
@@ -70,6 +106,16 @@ Partial Class Form1
         Me.hash.Name = "hash"
         Me.hash.Size = New System.Drawing.Size(374, 22)
         Me.hash.TabIndex = 1
+        '
+        'Restore
+        '
+        Me.Restore.Font = New System.Drawing.Font("Calibri", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Restore.Location = New System.Drawing.Point(491, 86)
+        Me.Restore.Name = "Restore"
+        Me.Restore.Size = New System.Drawing.Size(75, 26)
+        Me.Restore.TabIndex = 3
+        Me.Restore.Text = "Restore"
+        Me.Restore.UseVisualStyleBackColor = True
         '
         'HC
         '
@@ -120,29 +166,303 @@ Partial Class Form1
         '
         'comSent
         '
-        Me.comSent.Location = New System.Drawing.Point(21, 231)
+        Me.comSent.Location = New System.Drawing.Point(12, 330)
         Me.comSent.Name = "comSent"
         Me.comSent.Size = New System.Drawing.Size(544, 22)
         Me.comSent.TabIndex = 10
         Me.comSent.Visible = False
         '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 51)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(605, 273)
+        Me.TabControl1.TabIndex = 11
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Log)
+        Me.TabPage1.Controls.Add(Me.Benchmark)
+        Me.TabPage1.Controls.Add(Me.Restore)
+        Me.TabPage1.Controls.Add(Me.GroupBox1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(597, 244)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "DES"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Benchmark
+        '
+        Me.Benchmark.Font = New System.Drawing.Font("Calibri", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Benchmark.Location = New System.Drawing.Point(491, 118)
+        Me.Benchmark.Name = "Benchmark"
+        Me.Benchmark.Size = New System.Drawing.Size(75, 23)
+        Me.Benchmark.TabIndex = 4
+        Me.Benchmark.Text = "Bench"
+        Me.Benchmark.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.saveInfo)
+        Me.TabPage2.Controls.Add(Me.clearMailConfig)
+        Me.TabPage2.Controls.Add(Me.Label14)
+        Me.TabPage2.Controls.Add(Me.M7)
+        Me.TabPage2.Controls.Add(Me.saveMailData)
+        Me.TabPage2.Controls.Add(Me.M6)
+        Me.TabPage2.Controls.Add(Me.Label13)
+        Me.TabPage2.Controls.Add(Me.Label12)
+        Me.TabPage2.Controls.Add(Me.Label11)
+        Me.TabPage2.Controls.Add(Me.Label10)
+        Me.TabPage2.Controls.Add(Me.Label9)
+        Me.TabPage2.Controls.Add(Me.Label8)
+        Me.TabPage2.Controls.Add(Me.M5)
+        Me.TabPage2.Controls.Add(Me.M4)
+        Me.TabPage2.Controls.Add(Me.M3)
+        Me.TabPage2.Controls.Add(Me.M2)
+        Me.TabPage2.Controls.Add(Me.M1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(597, 244)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Notifications"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'saveInfo
+        '
+        Me.saveInfo.AutoSize = True
+        Me.saveInfo.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.saveInfo.ForeColor = System.Drawing.Color.ForestGreen
+        Me.saveInfo.Location = New System.Drawing.Point(502, 176)
+        Me.saveInfo.Name = "saveInfo"
+        Me.saveInfo.Size = New System.Drawing.Size(58, 18)
+        Me.saveInfo.TabIndex = 33
+        Me.saveInfo.Text = "Saved !!"
+        Me.saveInfo.Visible = False
+        '
+        'clearMailConfig
+        '
+        Me.clearMailConfig.Location = New System.Drawing.Point(494, 89)
+        Me.clearMailConfig.Name = "clearMailConfig"
+        Me.clearMailConfig.Size = New System.Drawing.Size(75, 28)
+        Me.clearMailConfig.TabIndex = 32
+        Me.clearMailConfig.Text = "Clear"
+        Me.clearMailConfig.UseVisualStyleBackColor = True
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(74, 179)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(59, 17)
+        Me.Label14.TabIndex = 31
+        Me.Label14.Text = "Subject:"
+        '
+        'M7
+        '
+        Me.M7.Location = New System.Drawing.Point(138, 208)
+        Me.M7.Name = "M7"
+        Me.M7.Size = New System.Drawing.Size(309, 22)
+        Me.M7.TabIndex = 30
+        '
+        'saveMailData
+        '
+        Me.saveMailData.Location = New System.Drawing.Point(481, 34)
+        Me.saveMailData.Name = "saveMailData"
+        Me.saveMailData.Size = New System.Drawing.Size(100, 40)
+        Me.saveMailData.TabIndex = 29
+        Me.saveMailData.Text = "Save"
+        Me.saveMailData.UseVisualStyleBackColor = True
+        '
+        'M6
+        '
+        Me.M6.Location = New System.Drawing.Point(139, 176)
+        Me.M6.Name = "M6"
+        Me.M6.Size = New System.Drawing.Size(308, 22)
+        Me.M6.TabIndex = 28
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(43, 83)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(89, 17)
+        Me.Label13.TabIndex = 27
+        Me.Label13.Text = "SMTP Client:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(40, 211)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(96, 17)
+        Me.Label12.TabIndex = 26
+        Me.Label12.Text = "MessageText:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(24, 51)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(110, 17)
+        Me.Label11.TabIndex = 25
+        Me.Label11.Text = "SenderAddress:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(15, 19)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(123, 17)
+        Me.Label10.TabIndex = 24
+        Me.Label10.Text = "RecipientAddress:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(61, 147)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(73, 17)
+        Me.Label9.TabIndex = 23
+        Me.Label9.Text = "Password:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(87, 115)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(47, 17)
+        Me.Label8.TabIndex = 22
+        Me.Label8.Text = "Login:"
+        '
+        'M5
+        '
+        Me.M5.Location = New System.Drawing.Point(138, 144)
+        Me.M5.Name = "M5"
+        Me.M5.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.M5.Size = New System.Drawing.Size(309, 22)
+        Me.M5.TabIndex = 21
+        '
+        'M4
+        '
+        Me.M4.Location = New System.Drawing.Point(138, 112)
+        Me.M4.Name = "M4"
+        Me.M4.Size = New System.Drawing.Size(309, 22)
+        Me.M4.TabIndex = 20
+        '
+        'M3
+        '
+        Me.M3.Location = New System.Drawing.Point(138, 80)
+        Me.M3.Name = "M3"
+        Me.M3.Size = New System.Drawing.Size(309, 22)
+        Me.M3.TabIndex = 19
+        '
+        'M2
+        '
+        Me.M2.Location = New System.Drawing.Point(138, 48)
+        Me.M2.Name = "M2"
+        Me.M2.Size = New System.Drawing.Size(309, 22)
+        Me.M2.TabIndex = 18
+        '
+        'M1
+        '
+        Me.M1.Location = New System.Drawing.Point(138, 16)
+        Me.M1.Name = "M1"
+        Me.M1.Size = New System.Drawing.Size(309, 22)
+        Me.M1.TabIndex = 17
+        '
+        'mailData
+        '
+        Me.mailData.Location = New System.Drawing.Point(12, 358)
+        Me.mailData.Name = "mailData"
+        Me.mailData.Size = New System.Drawing.Size(370, 161)
+        Me.mailData.TabIndex = 12
+        Me.mailData.Text = ""
+        Me.mailData.Visible = False
+        '
+        'TimerSaveOK
+        '
+        Me.TimerSaveOK.Interval = 1000
+        '
+        'Log
+        '
+        Me.Log.Location = New System.Drawing.Point(22, 100)
+        Me.Log.Name = "Log"
+        Me.Log.Size = New System.Drawing.Size(441, 125)
+        Me.Log.TabIndex = 5
+        Me.Log.Text = ""
+        '
+        'fileCodCheck
+        '
+        '
+        'Date_Now
+        '
+        Me.Date_Now.Location = New System.Drawing.Point(456, 358)
+        Me.Date_Now.Name = "Date_Now"
+        Me.Date_Now.Size = New System.Drawing.Size(100, 22)
+        Me.Date_Now.TabIndex = 13
+        Me.Date_Now.Visible = False
+        '
+        'DesCode
+        '
+        Me.DesCode.Location = New System.Drawing.Point(388, 386)
+        Me.DesCode.Name = "DesCode"
+        Me.DesCode.Size = New System.Drawing.Size(229, 133)
+        Me.DesCode.TabIndex = 14
+        Me.DesCode.Text = ""
+        Me.DesCode.Visible = False
+        '
+        'FileL
+        '
+        Me.FileL.Location = New System.Drawing.Point(585, 358)
+        Me.FileL.Name = "FileL"
+        Me.FileL.Size = New System.Drawing.Size(28, 22)
+        Me.FileL.TabIndex = 15
+        Me.FileL.Visible = False
+        '
+        'Datetxt
+        '
+        Me.Datetxt.AutoSize = True
+        Me.Datetxt.Location = New System.Drawing.Point(408, 361)
+        Me.Datetxt.Name = "Datetxt"
+        Me.Datetxt.Size = New System.Drawing.Size(42, 17)
+        Me.Datetxt.TabIndex = 16
+        Me.Datetxt.Text = "Date:"
+        Me.Datetxt.Visible = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(603, 153)
+        Me.AutoSize = True
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(620, 632)
+        Me.Controls.Add(Me.Datetxt)
+        Me.Controls.Add(Me.FileL)
+        Me.Controls.Add(Me.DesCode)
+        Me.Controls.Add(Me.Date_Now)
+        Me.Controls.Add(Me.mailData)
         Me.Controls.Add(Me.comSent)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.xNC2)
         Me.Controls.Add(Me.xNC1)
         Me.Controls.Add(Me.HC)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "DESBF v1.00"
+        Me.Text = "DESBF v1.01"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -158,4 +478,34 @@ Partial Class Form1
     Friend WithEvents Label3 As Label
     Friend WithEvents hash As TextBox
     Friend WithEvents comSent As TextBox
+    Friend WithEvents Restore As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Benchmark As Button
+    Friend WithEvents saveInfo As Label
+    Friend WithEvents clearMailConfig As Button
+    Friend WithEvents Label14 As Label
+    Friend WithEvents M7 As TextBox
+    Friend WithEvents saveMailData As Button
+    Friend WithEvents M6 As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents M5 As TextBox
+    Friend WithEvents M4 As TextBox
+    Friend WithEvents M3 As TextBox
+    Friend WithEvents M2 As TextBox
+    Friend WithEvents M1 As TextBox
+    Friend WithEvents mailData As RichTextBox
+    Friend WithEvents TimerSaveOK As Timer
+    Friend WithEvents Log As RichTextBox
+    Friend WithEvents fileCodCheck As Timer
+    Friend WithEvents Date_Now As TextBox
+    Friend WithEvents DesCode As RichTextBox
+    Friend WithEvents FileL As TextBox
+    Friend WithEvents Datetxt As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,17 +1,16 @@
 ﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Mail
 
 Public Class Form1
     Private Sub StartBF_Click(sender As Object, e As EventArgs) Handles StartBF.Click
+        Date_Now.Text = Date.Today
         Dim CommandX As String
-        CommandX = HC.Text + " -m 1500 " + hash.Text + " -a 3 ?1?1?1?1?1?1?1?1 -1 ?l?u?d -2 ?l?u --outfile=DES_Code\" + Date.Today + "_hash.txt --session des"
-        Dim ksMax As String
-        ksMax = "916132832"
-        Dim ks2 As String
-        ks2 = "458066416"
-        Dim ks3 As String
-        ks3 = "305377611"
-        Dim ks31 As String
-        ks31 = "610755222"
+        CommandX = HC.Text + " -m 1500 " + hash.Text + " -a 3 ?1?1?1?1?1?1?1?1 -1 ?l?u?d -2 ?l?u --outfile=DES_Code\" + Date_Now.Text + "_" + FileL.Text + "_Code.txt - -session des"
+        Dim ksMax As String = "916132832"
+        Dim ks2 As String = "458066416"
+        Dim ks3 As String = "305377611"
+        Dim ks31 As String = "610755222"
 
         If hash.Text.Length = 0 Then
             MsgBox("Please enter Hash !!")
@@ -43,6 +42,10 @@ Public Class Form1
                 comSent.Text = CommandX + " -s " + ks31 + " -l " + ksMax
             End If
         End If
+        If M1.Text.Length = 0 Or M2.Text.Length = 0 Or M3.Text.Length = 0 Or M4.Text.Length = 0 Or M5.Text.Length = 0 Or M6.Text.Length = 0 Or M7.Text.Length = 0 Then
+        Else
+            fileCodCheck.Enabled = True
+        End If
         'hashcat -m 1500 UGCfzJmLNga36 -a 3 ?1?1?1?1?1?1?1?1 -1 ?l?u?d -2 ?l?u --outfile=_hash.txt --session des
     End Sub
 
@@ -53,6 +56,18 @@ Public Class Form1
         Else
             Directory.CreateDirectory("DES_Code")
         End If
+        If My.Computer.FileSystem.FileExists("Mail_Data.tds") Then
+            mailData.LoadFile("Mail_Data.tds", RichTextBoxStreamType.PlainText)
+            M1.Text = mailData.Lines(0)
+            M2.Text = mailData.Lines(1)
+            M3.Text = mailData.Lines(2)
+            M4.Text = mailData.Lines(3)
+            M5.Text = mailData.Lines(4)
+            M6.Text = mailData.Lines(5)
+            M7.Text = mailData.Lines(6)
+        End If
+        Dim FileNo As Integer = Directory.GetFiles("DES_Code").Length
+        FileL.Text = FileNo + 1
     End Sub
 
     Private Sub NC1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles xNC1.SelectedIndexChanged
@@ -77,11 +92,112 @@ Public Class Form1
     Private Sub hash_TextChanged(sender As Object, e As EventArgs) Handles hash.TextChanged
         If hash.Text = "mobista" Then
             comSent.Visible = True
+            Datetxt.Visible = True
+            Date_Now.Visible = True
+            DesCode.Visible = True
+            FileL.Visible = True
+            mailData.Visible = True
             hash.Clear()
         End If
     End Sub
 
-    Private Sub HC_SelectedIndexChanged(sender As Object, e As EventArgs) Handles HC.SelectedIndexChanged
+    Private Sub Restore_Click(sender As Object, e As EventArgs) Handles Restore.Click
+        Process.Start("cmd", "/k " + HC.Text + " --session=des --restore")
+    End Sub
 
+    Private Sub Benchmark_Click(sender As Object, e As EventArgs) Handles Benchmark.Click
+        Process.Start("cmd", "/k " + HC.Text + " -b")
+    End Sub
+
+    Private Sub saveMailData_Click(sender As Object, e As EventArgs) Handles saveMailData.Click
+        If M1.Text.Length = 0 Or M2.Text.Length = 0 Or M3.Text.Length = 0 Or M4.Text.Length = 0 Or M5.Text.Length = 0 Or M6.Text.Length = 0 Or M7.Text.Length = 0 Then
+            MsgBox("Please check all textbox !!")
+        Else
+            If My.Computer.FileSystem.FileExists("Mail_Data.tds") Then
+                My.Computer.FileSystem.DeleteFile("Mail_Data.tds")
+                My.Computer.FileSystem.WriteAllText("Mail_Data.tds", M1.Text + vbCrLf + M2.Text + vbCrLf + M3.Text + vbCrLf + M4.Text + vbCrLf + M5.Text + vbCrLf + M6.Text + vbCrLf + M7.Text, True)
+                mailData.Clear()
+                mailData.LoadFile("Mail_Data.tds", RichTextBoxStreamType.PlainText)
+                TimerSaveOK.Enabled = True
+                saveInfo.Visible = True
+            Else
+                My.Computer.FileSystem.WriteAllText("Mail_Data.tds", M1.Text + vbCrLf + M2.Text + vbCrLf + M3.Text + vbCrLf + M4.Text + vbCrLf + M5.Text + vbCrLf + M6.Text + vbCrLf + M7.Text, True)
+                mailData.Clear()
+                mailData.LoadFile("Mail_Data.tds", RichTextBoxStreamType.PlainText)
+                TimerSaveOK.Enabled = True
+                saveInfo.Visible = True
+            End If
+        End If
+    End Sub
+
+    Private Sub TimerSaveOK_Tick(sender As Object, e As EventArgs) Handles TimerSaveOK.Tick
+        TimerSaveOK.Enabled = False
+        saveInfo.Visible = False
+    End Sub
+
+    Private Sub clearMailConfig_Click(sender As Object, e As EventArgs) Handles clearMailConfig.Click
+        If My.Computer.FileSystem.FileExists("Mail_Data.tds") Then
+            My.Computer.FileSystem.DeleteFile("Mail_Data.tds")
+            mailData.Clear()
+            M1.Clear()
+            M2.Clear()
+            M3.Clear()
+            M4.Clear()
+            M5.Clear()
+            M6.Clear()
+            M7.Clear()
+        Else
+        End If
+    End Sub
+
+    Private Sub fileCodCheck_Tick(sender As Object, e As EventArgs) Handles fileCodCheck.Tick
+        If My.Computer.FileSystem.FileExists("DES_Code\" + Date_Now.Text + "_" + FileL.Text + "_Code.txt") Then
+            fileCodCheck.Enabled = False
+
+            '***********************************************************************************************************************
+            Try
+                Dim mailfrom As New MailAddress(M1.Text, "DESBF") ' adres mail do wysyłki + nazwa
+                Dim mailto As New MailAddress(M2.Text, "BM") ' adres docelowy + nazwa
+                Dim message As New MailMessage(mailfrom, mailto)
+                Dim smtp As New SmtpClient(M3.Text) 'serwer smtp
+                Dim zaloncznik As String
+                zaloncznik = "DES_Code\" + Date_Now.Text + "_" + FileL.Text + "_Code.txt" 'nazwa pliku
+                If File.Exists(zaloncznik) Then
+                    Dim data As New Attachment(zaloncznik)
+                    message.Attachments.Add(data)
+                End If
+
+                message.Subject = M6.Text 'temat wiadomości
+
+
+                message.BodyEncoding = System.Text.Encoding.UTF8
+                message.Body = M7.Text 'tekst wiadomości
+
+                smtp.Credentials = New NetworkCredential(M4.Text, M5.Text) 'login i hasło
+
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network
+
+                smtp.Send(message)
+
+                Log.AppendText(Environment.NewLine)
+                Log.AppendText(Environment.NewLine + "Mail has been sent")
+
+
+            Catch ex As SmtpException
+                Log.AppendText(Environment.NewLine + ex.Message)
+            End Try
+
+            DesCode.LoadFile("DES_Code\" + Date_Now.Text + "Code.txt", RichTextBoxStreamType.PlainText)
+            Log.AppendText(Environment.NewLine + "Reading data...")
+            Log.AppendText(Environment.NewLine)
+            Log.AppendText(Environment.NewLine + "Master Code: ")
+            Log.SelectionColor = Color.DarkBlue
+            Log.AppendText(Mid(DesCode.Text, 66, 10))
+            Log.SelectionColor = Color.Empty
+            Log.AppendText(Environment.NewLine + "Hash: " + Mid(DesCode.Text, 1, 40))
+
+        Else
+
+        End If
     End Sub
 End Class
